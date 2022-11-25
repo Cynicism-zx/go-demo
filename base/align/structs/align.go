2470d0d44adb51015073b3fb1dd9681c51d4f6f5
lwj <soulguys@163.com>
2022-11-25 17:53:23 +0800

    feat: go内存对齐工具

---
+++ base/align/structs/align.go
@@ -0,0 +1,11 @@
+package structs
+
+type Post struct {
+	IsDraft     bool   // 1 byte
+	Title       string // 16 bytes
+	ID          int64  // 8 bytes
+	Description string // 16 bytes
+	IsDeleted   bool   // 1 byte
+	Author      string // 16 bytes
+	//CreatedAt   time.Time // 24 bytes
+}
